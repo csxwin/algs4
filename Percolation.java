7f1ae65e0d6ecc6b0caa7f457ac39193ce6b3681
Shuxun Cao <caoshuxun@gmail.com>
2016-10-10 16:56:02 -0400

    functional version 1
    fully functional but failed the coursera compiler

--- Percolation.java
+++ Percolation.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.StdRandom;
-//import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
     private int[] Open_node;
@@ -10,8 +10,11 @@ public class Percolation {
 
     public Percolation(int n)               // create n-by-n grid, with all sites blocked
     {
+        if(n <= 0)
+        {
+            throw new java.lang.IllegalArgumentException("n less than 1");
+        }
         N = n;
-        
         Open_node = new int[N * N + 2];
         for(int i = 1; i <= N * N; i++)
         {
@@ -23,6 +26,11 @@ public class Percolation {
     }
    public void open(int i, int j)          // open site (row i, column j) if it is not open already
    {
+       if((i < 1) || (i > N ) || (j < 1) || (j > N))
+       {
+           String exce = "i: " + i + " j: " + j + " out of boundary\n";
+           throw new IndexOutOfBoundsException(exce);
+       }
        if(isOpen(i, j) == false)
        {
            Open_node[(i - 1) * N + j] = 1;
@@ -34,19 +42,19 @@ public class Percolation {
            {
                UF.union(N * N + 1, (i - 1) * N + j);
            }
-           if(isOpen(i - 1, j) == true)
+           if((i > 1) && isOpen(i - 1, j))
            {
                UF.union((i - 1) * N + j, (i - 2) * N + j);
            }
-           if(isOpen(i + 1, j) == true)
+           if((i < N) && isOpen(i + 1, j))
            {
                UF.union((i - 1) * N + j, i * N + j);
            }
-           if(isOpen(i, j - 1) == true)
+           if((j > 1) && isOpen(i, j - 1))
            {
                UF.union((i - 1) * N + j, (i - 1) * N + j - 1);
            }
-           if(isOpen(i, j + 1) == true)
+           if((j < N) && isOpen(i, j + 1))
            {
                UF.union((i - 1) * N + j, (i - 1) * N + j + 1);
            }
@@ -55,9 +63,10 @@ public class Percolation {
    }
    public boolean isOpen(int i, int j)     // is site (row i, column j) open?
    {
-       if((i == 0)||(i == N + 1)||(j == 0)||(j == N + 1))
+       if((i < 1) || (i > N ) || (j < 1) || (j > N))
        {
-           return false;
+           String exce = "i: " + i + " j: " + j + " out of boundary\n";
+           throw new IndexOutOfBoundsException(exce);
        }
        else if(Open_node[(i - 1) * N + j] == 1)
        {
@@ -69,31 +78,54 @@ public class Percolation {
    }
    public boolean isFull(int i, int j)     // is site (row i, column j) full?
    {
+       if((i < 1) || (i > N ) || (j < 1) || (j > N))
+       {
+           String exce = "i: " + i + " j: " + j + " out of boundary\n";
+           throw new IndexOutOfBoundsException(exce);
+       }
        return UF.connected(0, (i - 1) * N + j);   // use 0 to represent top root       
    }
 
    public boolean percolates()             // does the system percolate?
    {
-       return isFull(N, N+1);     
+       return UF.connected(0, N * N + 1);     
    } 
    public static void main(String[] args)  // test client (optional)
    {
-       Percolation P = new Percolation(20);
+       Percolation P = new Percolation(10);
        int open_site = 0;
        int random_N;   
+       int[] exist_Site = new int[P.N * P.N + 1];
+       int i, j;
        while(P.percolates() == false)
        {
-           open_site++;
            if(open_site > P.N * P.N){break;}
            do{
                random_N = (int)(StdRandom.random() * P.N * P.N);
-               //random_N = 4;
            }while(random_N == 0);
-           //random_N = (open_site - 1) * P.N + 1; //vertical drill
-           //System.out.format("%d\ni:%d j:%d   counter:%d\n", random_N, (int)(random_N / P.N) + 1, (int)(random_N % P.N), open_site);      
-           P.open((int)(random_N / P.N) + 1, (int)(random_N % P.N));
+           boolean IsExist = false;
+           for(int array_Index = 1; array_Index <= open_site;   array_Index++)
+           {
+               if(random_N == exist_Site[array_Index])
+               {
+                   IsExist = true;
+               }
+           }
+           if(!IsExist){
+               open_site++;
+               exist_Site[open_site] = random_N;
+           }
+           if((int)(random_N % P.N) == 0){
+               i = random_N / P.N;
+               j = P.N;
+           }
+           else{
+               i = random_N / P.N + 1;
+               j = random_N % P.N;
+           }    
+           P.open(i, j);
        }
        P.Threshold = open_site / (double)(P.N * P.N);
-       System.out.format("\n%f\n", P.Threshold);
+       StdOut.printf("\n%f\n", P.Threshold);
    }
 }
