2b0842386a0c0e3b404b74eea5973eedb167ab9b
csxwin <caoshuxun@gmail.com>
2017-06-07 00:13:11 -0400

    add iterator functions

--- chapter4/Board.java
+++ chapter4/Board.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.MinPQ; 
+import java.util.Iterator;
+
 public class Board {
     private int[][] locBlocks = new int[3][3];
     private int dimension;
@@ -14,7 +17,7 @@ public class Board {
             for (int j = 0; j < blocks[0].length; j++)
             {
                 locBlocks[i][j] = blocks[i][j];
-                if (locBlocks[i][j] = 0)
+                if (locBlocks[i][j] == 0)
                 {
                     //init location of blank
                     blankX = i;
@@ -31,22 +34,54 @@ public class Board {
     }
     public int hamming()                   // number of blocks out of place
     {
-        return 0;
+        int numOfOut = 0;
+        for (int i = 0; i < dimension; i++)
+        {
+            for (int j = 0; j < dimension; j++)
+            {
+                if (locBlocks[i][j] != 0)
+                {
+                    if (locBlocks[i][j] != i * dimension + j + 1)
+                    {
+                        numOfOut++;
+                    }
+                }
+            }
+        }
+        return numOfOut;
     }
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
     {
-        return 0;
+        int numOfOut = 0;
+        for (int i = 0; i < dimension; i++)
+        {
+            for (int j = 0; j < dimension; j++)
+            {
+                if (locBlocks[i][j] != 0)
+                {
+                    if (locBlocks[i][j] != i * dimension + j + 1)
+                    {
+                        numOfOut += i - (locBlocks[i][j] - 1) % dimension
+                            + j - (locBlocks[i][j] - 1) / dimension;
+                    }
+                }
+            }
+        }
+        return numOfOut;
     }
     public boolean isGoal()                // is this board the goal board?
     {
         boolean isGoal = true;
-        for (int i = 0; i < blocks[0].length; i++)
+        for (int i = 0; i < dimension; i++)
         {
-            for (int j = 0; j < blocks[0].length; j++)
+            for (int j = 0; j < dimension; j++)
             {
-                if (locBlocks[i][j] != i * dimension + j + 1)
+                if (!(i == 2 & j == 2))
                 {
-                    isGoal = false;
+                    if (locBlocks[i][j] != i * dimension + j + 1)
+                    {
+                        isGoal = false;
+                    }
                 }
             }
         }
@@ -54,14 +89,41 @@ public class Board {
     }
     public Board twin()                    // a board that is obtained by exchanging any pair of blocks
     {
-        Board newBoard;
+        int[][] twinBlock = new int[3][3];
+        twinBlock = locBlocks;
+        Board newBoard = new Board(twinBlock);
         return newBoard;
     }
     public boolean equals(Object y)        // does this board equal y?
     {
         return true;
     }
-//    public Iterable<Board> neighbors()     // all neighboring boards
+    public Iterable<Board> neighbors()     // all neighboring boards
+    {
+        return new BoardIterator();
+    }
+    private class BoardIterator implements Iterable<Board>
+    {
+        public Iterator<Board> iterator()
+        {
+            return new BoardIterators();
+        }
+        private class BoardIterators implements Iterator<Board>
+        {
+            private Board currentBoard = new Board(locBlocks);
+            public boolean hasNext()
+            {
+                return true;
+            }
+            public void remove() {}
+            public Board next()
+            {
+                Board board = currentBoard.twin();
+                return board;
+            }
+        }
+    }
+    
 //    public String toString()               // string representation of this board (in the output format specified below)
 
     public static void main(String[] args) // unit tests (not graded)
@@ -76,7 +138,7 @@ public class Board {
         initBoard[2][0] = 7;
         initBoard[2][1] = 8;
         initBoard[2][2] = 6;
-        
+        Board testBoard = new Board(initBoard);
         
     }
 }
